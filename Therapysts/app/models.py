a235be8bda018b6662a660ccd753ff4d15357b7e
SilverSteelson <bolthold7507@gmail.com>
2021-02-26 02:19:52 +0300

    Скрипт работает с обновлённой схемой,где методики сохранены в отдельную таблицу и связаны с терапефтами связью многие-к-многим Если запись из терапефтов удалить она также удаляется  из таблицы связей

--- Therapysts/app/models.py
+++ Therapysts/app/models.py
@@ -3,12 +3,16 @@ Definition of models.
 """
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as trnsl
+#from django.utils.translation import ugettext_lazy as trnsl
+class methods(models.Model):
+    therapy = models.TextField(null=False,blank=False,unique = True)
+    def __init__(self):
+        return self.therapy
 
 class CLINICUS(models.Model):
     idRecords = models.CharField(max_length=20)
     name = models.CharField(max_length=60)
-    Methods = models.TextField(null=False,blank=False)
+    Methods = models.ManyToManyField(methods)
     urlsLrgeFoto = models.URLField()
     urlsSmlFoto = models.URLField()
     timeLoad = models.DateField(auto_now_add=True,db_index=True)
