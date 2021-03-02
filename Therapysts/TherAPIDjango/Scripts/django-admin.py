cf46ee81cbdb1b3fb40fc19616317603f9184d6e
SilverSteelson <bolthold7507@gmail.com>
2021-03-02 04:18:28 +0300

    Удалил как оказалось е удалёные проекты из гит репозитория

--- Therapysts/TherAPIDjango/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Bolt Hold\source\repos\PsyhologyTest\Therapysts\TherAPIDjango\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
